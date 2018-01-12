a5eb34d728f5c2491eddc7d78cbd877f9de8fade
nenad0212 <nenad.rakas0212@gmail.com>
2018-01-12 17:10:32 +1100

    using ngFor directive for looping data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,7 +8,25 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent{
 products : any[] = [
-        {},
-        {}
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling cart",
+            "price": 32.99,
+            "starRating":4.2,
+            "imageUrl":"#"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "March 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "#"
+        },
     ];
 }
