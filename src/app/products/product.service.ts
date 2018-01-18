03ccecd9379f894037030562d216afb520cb3e4f
nenad0212 <nenad.rakas0212@gmail.com>
2018-01-19 09:47:43 +1100

    hardcoded one more product

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -27,6 +27,17 @@ export class ProductService {
                 'starRating': 4.8,
     'imageUrl' : 'https://static.chemistwarehouse.com.au/ams/media/pi/70243/F2D_200.jpg'
             },
-        ];;
+            {
+                'productId': 6,
+                'productName': 'Healthy Care Propolis Toothpaste 120g',
+                'productCode': '00-0003',
+                'releaseDate': 'March 21, 2017',
+                'description': 'It not only protects your teeth from decay but it also promotes healthy gums and fresh breath.',
+                'price': 2.99,
+                'originPrice': 2.99,
+                'starRating': 4.5,
+    'imageUrl' : 'https://static.chemistwarehouse.com.au/ams/media/pi/59410/F2D_800.jpg'
+            },
+        ];
     }
 }
