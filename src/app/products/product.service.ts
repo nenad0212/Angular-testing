1c7e994f4e545f28c6f24fef3b3b87006afc0c8a
nenad0212 <nenad.rakas0212@gmail.com>
2018-01-19 15:19:05 +1100

    refine formatting

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,9 @@
-import { IProduct } from "./product";
+import { IProduct } from './product';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ProductService {
-    getProducts() : IProduct[]{
+    getProducts(): IProduct[] {
         return [
             {
                 'productId': 2,
