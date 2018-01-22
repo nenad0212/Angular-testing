75f52ee3d55a4a12d54a62a9b12b16bfe5e1d7e3
nenad0212 <nenad.rakas0212@gmail.com>
2018-01-22 12:02:08 +1100

    add observable error handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IProduct } from './product';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +13,17 @@ export class ProductService {
     }
 
     productsUrl = './api/products/products.json';
+
     getProducts(): Observable<IProduct[]> {
-      return this._http.get<IProduct[]>(this.productsUrl);
+      return this._http.get<IProduct[]>(this.productsUrl)
+      .do(
+        data => console.log('All: ' + JSON.stringify(data))
+      )
+      .catch(this.handleError);
+    }
+
+    private handleError(err: HttpErrorResponse) {
+      console.log(err.message);
+      return Observable.throw(err.message);
     }
 }
